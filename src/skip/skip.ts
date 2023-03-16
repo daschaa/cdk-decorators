abe05378bc65763c6b9956e96a7438150cb2c8be
Joshua Weber <josh@joshuaw.de>
2023-03-16 21:43:09 +0100

    chore: Refactors skip decorator to use empty class

--- src/skip/skip.ts
+++ src/skip/skip.ts
@@ -4,19 +4,16 @@ import { Stack } from 'aws-cdk-lib';
  * Decorator to skip all resources in a stack.
  *
  * If this is used on a class, it will remove all resources from the stack.
- * It's doing this by removing all nodes from the stack.
+ * It's doing this by replacing the stack with an empty stack.
  *
  * @example
  * @Skip
  */
-export function Skip<T extends { new (...args: any[]): Stack }>(ctor: T): any {
-  return class extends ctor {
-    constructor(...args: any[]) {
-      super(...args);
-      const children = this.node.findAll();
-      children.forEach((child) => {
-        this.node.tryRemoveChild(child.node.id);
-      });
+export function Skip(_: any): any {
+  class Empty extends Stack {
+    constructor() {
+      super();
     }
-  };
+  }
+  return Empty;
 }
